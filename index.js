23f023e333429b73b684dc44b38076531dda1f9b
Roy Scheffers <r.scheffers@gmail.com>
2018-12-08 21:31:08 +0100

    Add payload support to server

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 
-// Dependencies
-const http      = require('http');
-const url       = require('url');
+/*  
+ *
+ *  Primary file for API
+ *  
+ */ 
 
+
+// Dependencies
+const http              = require('http');
+const url               = require('url');
+const StringDecoder     = require('string_decoder').StringDecoder;
 
 // Configuration
 const port = 3000;
@@ -18,11 +25,38 @@ const server = http.createServer(function(req, res){
     const path = parsedUrl.pathname;
     const trimmedPath = path.replace(/^\/+|\/+$/g, '');         // clean path so /endpoint == /endpoint/
 
-    // Send the response
-    res.end('hello World\n');
+    // Get the query string as an object
+    const queryStringObject = parsedUrl.query;
 
-    // Log the request path
-    console.log('Request received on path: ', trimmedPath);
+    // Get the HTTP Method
+    const method = req.method.toLowerCase();
+
+    // Get the headers as an object
+    const headers = req.headers;
+
+    // Get the payload is there is any
+    const decoder = new StringDecoder('utf-8');
+    let buffer = '';
+
+    // Collect data as it comes in, only used then a payload is specified.
+    req.on('data', function(data){
+        buffer += decoder.write(data);
+    });
+
+    // When the end has reached, log the response.
+    // This method is always called regardless if there's a payload
+    req.on('end', function() {
+        buffer += decoder.end();
+
+        // Send the response
+        res.end('hello World\n');
+
+        // Log the request path
+        // console.log('Request received on path:', trimmedPath, 'with method:', method, 'with the following query string params', queryStringObject);
+        // console.log('Request includes the following headers:', headers);
+        console.log('Request was called with this payload: ', buffer);
+
+    })
 });
 
 // Start the server, and have it listen on port 3000
